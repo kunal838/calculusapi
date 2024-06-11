c52e9f3aa1b6fb84aa582699a26af628cf38e3e0
Your Name <you@example.com>
2024-06-11 17:54:44 +0530

    basic frontend added

--- learn.py
+++ learn.py
@@ -1,22 +1,22 @@
-from flask import *  
+from flask import *
 from sympy import *
+from flask_cors import CORS
 
 x, y = symbols('x y')
 
 app = Flask(__name__)  
+CORS(app)  # Enable CORS for all routes
 @app.route('/derivation/<string:n>')  
 def derivation(n):  
        
-   
-    
     expr = n
     expr_diff = Derivative(expr, x).doit() 
     k = str(expr_diff)
     m = {
-        
         "number":k
     }
     return jsonify(m)
+
 @app.route('/integrationwo/<string:m>')
 def integration(m):
     exp = m
@@ -27,21 +27,5 @@ def integration(m):
     }
     return jsonify(yo)
 
-@app.route('/integrationw/<string:e>/<string:ll>/<string:ul>')
-def integrationw(e,ll,ul):
-    exp = e
-    expr1 = integrate(e, (x,ll,ul)).doit()
-    kp = str(expr1)
-    yo = {
-        "number":kp
-    }
-    return jsonify(yo)
-
-
-
-
-
-  
-  
 if __name__ == '__main__':  
-    app.run(debug = True)  
+    app.run(debug=True)  
